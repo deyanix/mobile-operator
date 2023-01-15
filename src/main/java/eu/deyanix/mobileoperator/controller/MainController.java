c0de046f7f994a59b4d58bd36f4308fc98841072
deyanix <deyanix@deyanix.eu>
2023-01-15 23:46:46 +0100

    Added error pages

--- src/main/java/eu/deyanix/mobileoperator/controller/MainController.java
+++ src/main/java/eu/deyanix/mobileoperator/controller/MainController.java
@@ -1,8 +1,11 @@
 package eu.deyanix.mobileoperator.controller;
 
 import eu.deyanix.mobileoperator.repository.UserRepository;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
@@ -10,10 +13,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class SecurityController {
+public class MainController implements ErrorController {
     private UserRepository userRepository;
 
-    public SecurityController(UserRepository userRepository) {
+    public MainController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
@@ -30,5 +33,27 @@ public class SecurityController {
         }
         model.addAttribute("errorMessage", errorMessage);
         return "login";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            model.addAttribute("statusCode", statusCode);
+            model.addAttribute("status", HttpStatus.valueOf(statusCode));
+
+            if (statusCode == HttpStatus.UNAUTHORIZED.value()) {
+                return "main/error-401";
+            }
+            if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "main/error-403";
+            }
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "main/error-404";
+            }
+        }
+        return "main/error";
     }
 }
